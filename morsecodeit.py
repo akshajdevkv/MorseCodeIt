3308074e609965dc479b1b7f71ea38c2ea83ea51
Akshaj Dev <62797898+akshajdevkv@users.noreply.github.com>
2020-10-23 21:40:55 +0530

    Update morsecodeit.py

--- morsecodeit.py
+++ morsecodeit.py
@@ -52,4 +52,4 @@ def play_sound(encrypted_message,speed='medium'):
 
 message = morse_code_converter('Hello my name is akshaj dev and I created this program')
 print(message)
-play_sound(message)
+play_sound(message,'slow')
