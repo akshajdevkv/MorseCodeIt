5abcca5b0298f04efedc63d015a1586dbc7e6b3b
Akshaj Dev <62797898+akshajdevkv@users.noreply.github.com>
2020-10-24 09:41:02 +0530

    Update morsecodeit.py

--- morsecodeit.py
+++ morsecodeit.py
@@ -21,6 +21,7 @@ MORSE_CODE_DICT = {
 
 
 def morse_code_converter(message):
+    """Converts the text into morse"""
     encrypted_message = ''
     for letter in [i.upper() for i in message]:
         if letter in MORSE_CODE_DICT:
@@ -33,23 +34,29 @@ def morse_code_converter(message):
     
 
 def play_sound(encrypted_message,speed='medium'):
+    """
+    Plays the Morse Code using playsound.
+    3 speed options available: 'slow','medium','fast'.By default speed is set to default 
+    """
     timing = {
-    'slow' : [1.2,1.5],
-    'medium' : [0.75,1],
-    'fast' : [0.15,0.5]
+    'slow' : [0.25,1.2,1.5],#delay between (dot&dash), words and spaces respectively
+    'medium' : [0.05,0.75,1],
+    'fast' : [0,0.15,0.5]
     }
 
     for code in [i for i in encrypted_message ]:
         if code == '.':
             playsound.playsound('./dot.wav')
+            time.sleep(timing[speed][0])
         elif code == '-':
             playsound.playsound('./dash.wav')
+            time.sleep(0.25)
         elif code == ' ':
-            time.sleep(timing[speed][0])
+            time.sleep(timing[speed][1])
         elif code == '/':
-            time.sleep(timing[speed][1])    
+            time.sleep(timing[speed][2])    
+ 
 
-
-message = morse_code_converter('Hello my name is akshaj dev and I created this program')
+message = morse_code_converter('sms')
 print(message)
-play_sound(message,'slow')
+play_sound(message,speed='slow')
